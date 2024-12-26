9678cddb949d5f4a1d15f65bbaa808e64b359de3
Avneet Singh <avneetsingh843@gmail.com>
2024-12-26 17:49:04 +0530

    fix tabs

--- src/app/Components/Main/index.tsx
+++ src/app/Components/Main/index.tsx
@@ -20,29 +20,40 @@ const Main = () => {
 
   return (
     <div>
-      <div className="flex justify-between">
-        <p className="md:pl-[3%] pl-[5%] text-[#FAFAFA] mt-[1vh] md:mt-[5vh] text-[1rem] md:text-[2rem] font-[600] tracking-wider">
+      {/* Section Heading */}
+      <div className="flex flex-col md:flex-row">
+        <p className="basis-full md:basis-2/6 text-[#FAFAFA] mt-[1vh] md:mt-[5vh] text-[1rem] md:text-[2rem] font-[600] tracking-wider md:pl-[3%] pl-[5%]">
           {selectedCard}
         </p>
+
+        {/* Tabs Container */}
         <div
-          className={`z-[100] sidecard flex md:w-[57%] justify-between p-4 md:p-6 bg-[#282829] rounded-ss-2xl md:rounded-ss-none rounded-se-2xl border-[#303030] border-[1px] rounded-es-2xl tracking-wider font-[500]`}
+          className={`basis-full md:basis-4/6 z-[100] sidecard flex flex-wrap items-center p-4 md:p-6 bg-[#282829] rounded-ss-2xl md:rounded-ss-none rounded-se-2xl border-[#303030] border-[1px] rounded-es-2xl tracking-wider font-[500] mt-4 md:mt-0`}
         >
-          {tabs.map((tab) => (
-            <p
-              key={tab.name}
-              onClick={() => setSelectedCard(tab.name)}
-              className={`${
-                selectedCard === tab.name
-                  ? "text-[#FFDB70]"
-                  : "text-[#D6D6D6] hover:text-[#A2A2A2]"
-              } cursor-pointer text-[0.8rem] sm:text-[0.6rem] md:text-[1.1rem]`}
-            >
-              {tab.name}
-            </p>
+          {tabs.map((tab, index) => (
+            <div className="flex items-center" key={tab.name}>
+              <p
+                onClick={() => setSelectedCard(tab.name)}
+                className={`cursor-pointer text-[0.8rem] sm:text-[0.6rem] md:text-[1.1rem] ${
+                  selectedCard === tab.name
+                    ? "text-[#FFDB70]"
+                    : "text-[#D6D6D6] hover:text-[#A2A2A2]"
+                }`}
+              >
+                {tab.name}
+              </p>
+              {index < tabs.length - 1 && (
+                <span className="mx-2 text-[#D6D6D6]">|</span>
+              )}
+            </div>
           ))}
         </div>
       </div>
+
+      {/* Underline */}
       <hr className="bg-[#FFDB70] rounded-lg h-[0.3rem] ml-[5%] md:ml-[3%] w-[7%] mt-[0.3vh] md:mt-[1.7vh] border-0" />
+
+      {/* Render Selected Component */}
       {tabs.find((tab) => tab.name === selectedCard)?.component}
     </div>
   );
